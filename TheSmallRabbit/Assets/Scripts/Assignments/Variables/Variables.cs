c64fa6da36fce5102f2d8829bbf066dd1afc669e
Marshall Krueger <mgkrueger4@gmail.com>
2020-01-13 21:31:08 -0700

    added rabbit characteristic variables aNd a box objEct that has it's color set by a VariablE in the vaRialbes.cs script

--- TheSmallRabbit/Assets/Scripts/Assignments/Variables/Variables.cs
+++ TheSmallRabbit/Assets/Scripts/Assignments/Variables/Variables.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
-    // Declaration
-    int numberOne;
-    //Declaration and initialization
-    int numberTwo = 20;
+    // Rabbit data
+    string rabbitName;
+    int maxHealth;
+    int maxHunger = 20;
+    int currentHealth;
+    int currentHunger;
+    Color coloration;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rabbitName = "Peter";
+        maxHealth = 10;
+        currentHealth = maxHealth;
+        currentHunger = maxHunger;
+        coloration = new Color(.1f, .1f, .1f);
+        GetComponent<Renderer>().material.color = coloration;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForceAtPosition(Vector3.up * 200, gameObject.transform.position);
+        }
     }
 }
