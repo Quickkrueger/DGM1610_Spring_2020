717e73cd4d7e2368b2b4f192bea11e6b58c879c3
Marshall Krueger <mgkrueger4@gmail.com>
2020-01-13 17:14:04 -0700

    Added varialbes script

---
+++ TheSmallRabbit/Assets/Scripts/Assignments/Variables/Variables.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // Declaration
+    int numberOne;
+    //Declaration and initialization
+    int numberTwo = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
