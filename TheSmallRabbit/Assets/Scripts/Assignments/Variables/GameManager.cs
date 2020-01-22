505c243d1462f8786a96933e38f39ab7f3d5f604
Marshall Krueger <mgkrueger4@gmail.com>
2020-01-21 21:46:43 -0700

    added variables to gamemaNagEr in order to track important ValuEs as the playeR plays the game.

--- TheSmallRabbit/Assets/Scripts/Assignments/Variables/GameManager.cs
+++ TheSmallRabbit/Assets/Scripts/Assignments/Variables/GameManager.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public static GameManager instance;
+    private int score = 0;
+    public Text scoreText;
+    public Slider hungerBar;
+    public Slider healthBar;
     void Start()
     {
         instance = GetComponent<GameManager>();
+        scoreText.text = score.ToString();
     }
 
     // Update is called once per frame
