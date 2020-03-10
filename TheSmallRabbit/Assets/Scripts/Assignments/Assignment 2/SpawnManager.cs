d78a4cb4ef83b67bc5b4cded2b91ecb0a0cb9860
Marshall Krueger <mgkrueger4@gmail.com>
2020-03-10 09:45:39 -0600

    Made the fox a prefab, adjusted the spawn manager

--- TheSmallRabbit/Assets/Scripts/Assignments/Assignment 2/SpawnManager.cs
+++ TheSmallRabbit/Assets/Scripts/Assignments/Assignment 2/SpawnManager.cs
@@ -6,7 +6,8 @@ public class SpawnManager : MonoBehaviour
 {
     //TODO: Add ability to spawn more than just one type of enemy.
     public GameObject[] enemyPrefabs;
-    public int enemyIndex;
+    public int spawnLimit = 5;
+    int enemiesSpawned = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +17,11 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.LeftControl) && enemiesSpawned < spawnLimit)
         {
+            int enemyIndex = Random.Range(0, enemyPrefabs.Length);
             Instantiate(enemyPrefabs[enemyIndex], new Vector3(0, 0, 0), enemyPrefabs[enemyIndex].transform.rotation);
+            enemiesSpawned++;
         }
     }
 }
