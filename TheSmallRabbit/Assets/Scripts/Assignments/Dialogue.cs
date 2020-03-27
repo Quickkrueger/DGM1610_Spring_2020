3dfb4cc81e58f82f54709b6d875e968081bbaf20
Marshall Krueger <mgkrueger4@gmail.com>
2020-03-27 16:51:13 -0600

    Started some very basic npc dialogue mechanics

---
+++ TheSmallRabbit/Assets/Scripts/Assignments/Dialogue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue
+{
+    private List<string> lines;
+    private int currentLineIndex;
+
+    Dialogue()
+    {
+        currentLineIndex = 0;
+        lines = new List<string>();
+    }
+
+    public string GetCurrentLine()
+    {
+        if (currentLineIndex < lines.Count)
+        {
+            return lines[currentLineIndex];
+        }
+        return "";
+    }
+
+    public void AdvanceDialogue()
+    {
+        currentLineIndex++;
+    }
+
+    public void LoadDialogue(string characterName)
+    {
+
+    }
+}
---
+++ TheSmallRabbit/Assets/Scripts/Assignments/NPC.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Text dialogueText;
+    public string characterName = "Mario";
+    Dialogue dialogue;
+    void Start()
+    {
+        dialogue.LoadDialogue(characterName);
+    }
+
+    public void Speak()
+    {
+        dialogueText.text = dialogue.GetCurrentLine();
+        dialogue.AdvanceDialogue();
+    }
+    
+}
