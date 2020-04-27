ae615ef8edbb2e2b88e583465aa2d9d383337e01
Marshall Krueger <mgkrueger4@gmail.com>
2020-04-27 16:33:00 -0600

    Added a means of exiting the game, lowered music volume

--- Farmers Vs Aliens/Assets/Scripts/Managers/GameManager.cs
+++ Farmers Vs Aliens/Assets/Scripts/Managers/GameManager.cs
@@ -31,6 +31,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            Application.Quit();
+        }
+
         if (SpawnManager._instance.NoMoreCows())
         {
             GameOver(0);
